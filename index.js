6ddc803af019c078e8146aebe14b4b069ceeea78
unknown <janeaustine0@gmail.com>
2022-01-20 11:28:04 +0600

    discover fly second part done

--- index.js
+++ index.js
@@ -1,60 +1,35 @@
-document.getElementById("second-increase").addEventListener("click", function () {
-    const currentPerson = document.getElementById("second-person");
-    const newPerson = parseInt(currentPerson.value);
-    const totalPerson = newPerson + 1;
-    currentPerson.value = totalPerson;
-  });
-document.getElementById("second-increase").addEventListener("click", function () {
-    const currentPerson = document.getElementById("second-person");
-    const newPerson = parseInt(currentPerson.value);
-    const totalPerson = newPerson - 1;
-    currentPerson.value = totalPerson;
-  });
-
-
-  function personHandler(person,increase){
-      const newPerson = getInputValue(person);
-      if(increase == true){
-      totalPerson = newPerson + 1;
-      }
-      if(increase == false && newPerson > 0){
-      totalPerson = newPerson - 1;
-      }
-      document.getElementById(person +'-person').value = totalPerson;
-      if(person == 'first'){
-      totalNewPerson = totalPerson * 150;
-      }
-      if(person == 'second'){
-      totalNewPerson = totalPerson * 100;
-      }
-      document.getElementById("total-amount").innerText = '$' + totalNewPerson;
-      const totalVat = totalNewPerson * 0.1;
-      document.getElementById("total-vat").innerText = '$' + totalVat;
-      const grandAmount = totalNewPerson + totalVat;
-      document.getElementById("grand-amount").innerText = '$' + grandAmount;
+function personHandler(person, increase) {
+  const newPerson = getInputValue(person);
+  let totalPerson = 0;
+  if (increase == true) {
+    totalPerson = newPerson + 1;
   }
+  if (increase == false && newPerson > 0) {
+    totalPerson = newPerson - 1;
+  }
+  document.getElementById(person + "-person").value = totalPerson;
+  const inputPersonFirst = getInputValue("first");
+  const inputPersonSecond = getInputValue("second");
+  const allPerson = inputPersonFirst * 150 + inputPersonSecond * 100;
+  document.getElementById("total-amount").innerText = "$" + allPerson;
+  calculateTotal();
+}
+
+function calculateTotal() {
+  const inputPersonFirst = getInputValue("first");
+  const inputPersonSecond = getInputValue("second");
+  const allPerson = inputPersonFirst * 150 + inputPersonSecond * 100;
+  const totalVat = allPerson * 0.1;
+  document.getElementById("total-vat").innerText = "$" + totalVat;
+  const grandAmount = allPerson + totalVat;
+  document.getElementById("grand-amount").innerText = "$" + grandAmount;
+}
+
 function getInputValue(person) {
   const productInput = document.getElementById(person + "-person");
   const productCount = parseInt(productInput.value);
   return productCount;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
